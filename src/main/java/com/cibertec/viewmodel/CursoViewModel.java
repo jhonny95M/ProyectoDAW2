582ccf7261891e4b0039826931b2f6454d3d62de
Jhony Malpartida <juniorjhonnymalpartida@gmail.com>
2023-06-02 00:19:06 -0500

    agregado de propirdad curso view model

--- src/main/java/com/cibertec/viewmodel/CursoViewModel.java
+++ src/main/java/com/cibertec/viewmodel/CursoViewModel.java
@@ -1,5 +1,13 @@
 package com.cibertec.viewmodel;
 
 public class CursoViewModel {
+	private String nomcurso;
 
+	public String getNomcurso() {
+		return nomcurso;
+	}
+
+	public void setNomcurso(String nomcurso) {
+		this.nomcurso = nomcurso;
+	}
 }
